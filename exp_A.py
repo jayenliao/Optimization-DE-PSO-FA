ffbd9d2307bbe9d7d1629b0a6072810f5c3d436f
jayenliao <jay.chiehen@gmail.com>
2021-06-01 16:27:12 +0800

    Update exp_A.py and upload new outputs

--- exp_A.py
+++ exp_A.py
@@ -2,21 +2,23 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from args import init_arguments
 
 def find_folders(N, t, ROOT='./output/'):
     lst = os.listdir('./output/')
     key = 'N='+str(N)+'_t='+str(t)
     out = []
     for x in lst:
-        h = x.split('-')[3]
-        if key in x and int(h) >= 21:
-            out.append(ROOT + x)
+        if '.' not in x:
+            h = x.split('-')[3]
+            if key in x and int(h) >= 21:
+                out.append(ROOT + x)
     return out
 
-def plot_algorithms(N):
+def plot_algorithms(N, ROOT):
 
     d_globalv = {}
-    roots = find_folders(N, 50)
+    roots = find_folders(N, 50, ROOT)
 
     for i, fn in enumerate(roots):
         fn = fn + '/' if fn[-1] != '/' else fn
@@ -39,6 +41,10 @@ def plot_algorithms(N):
     plt.grid(linestyle='--')
 
 if __name__ == '__main__':
+    ROOT = init_arguments().parse_args().savePATH
+    print('The convergence plot are saved as:')
     for N in (10, 50, 100, 200):
-        plot_algorithms(N)
-        plt.show()
+        plot_algorithms(N, ROOT)
+        fn = ROOT + 'convergence_plot_N=' + str(N) + '.png'
+        plt.savefig(fn)
+        print('-->', fn)
